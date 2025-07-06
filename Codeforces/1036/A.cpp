eddc9985cb334053cce40f7fc55a0242eeec74e3
Hassan Chowdhry <94680098+HassanChowdhry@users.noreply.github.com>
2025-07-06 11:08:47 -0300

    re-order folders

---
+++ Codeforces/1036/A.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define fs first 
+#define sc second 
+#define all(a) a.begin(),a.end()
+#define mod(num, n) (num % n + n) % n
+#define flash ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define test int t;cin>>t;while(t--)
+#define rep(i,n) for(int i=0;i<n;++i)
+#define repx(i,x,n) for(int i=x;i<n;++i)
+#define per(i,n) for(int i=n-1;i>=0;--i)
+#define input(arr,n);for(int i=0;i<n;++i)cin>>arr[i]
+#define rem(mp, el) if(mp[el]==0)mp.erase(el)
+#define ln "\n"
+#define vi vector<int>
+#define vl vector<ll>
+int MOD=1000000007;
+
+void solve() {}
+
+int main() {
+  flash;
+  solve();
+  return 0;
+}
