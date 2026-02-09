e3fe1162f659ca9f237bf87ba98ee442d2f2b896
Hassan Chowdhry <94680098+HassanChowdhry@users.noreply.github.com>
2026-02-08 23:01:32 -0800

    CF

---
+++ Codeforces/4/A/A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  int n; cin >> n;
+
+  if (n > 3 && n % 2 == 0) cout << "YES";
+  else cout << "NO";
+}
+
+int main() {
+  solve();
+  return 0;
+}
